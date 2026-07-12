24ebf0cd16129d94185f6b5d266bf3086c727898
dimitris <dimitris@papergo.io>
2026-07-12 15:06:59 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/drone-site/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
